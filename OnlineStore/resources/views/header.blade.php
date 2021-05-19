9cd3f649968c498fb59a037cf06f7ca9a3e72d86
Bekzod <bbkxaladin@gmail.com>
2021-05-19 22:21:54 +0500

    User's Log In page is ready

--- OnlineStore/resources/views/header.blade.php
+++ OnlineStore/resources/views/header.blade.php
@@ -26,7 +26,7 @@
                 <button type="submit" class="btn btn-default">Submit</button>
             </form>
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">Cart(0)</a></li>
+                <li><a href="#">Orders(0)</a></li>
             </ul>
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
